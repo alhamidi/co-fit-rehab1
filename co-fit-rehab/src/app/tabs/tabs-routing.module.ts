0d9548827498246f6028b3c051cfaf85f6cd411a
nofaulia <nofaulia@gmail.com>
2021-07-01 23:31:00 +0700

    rename tab2 to latihan

--- co-fit-rehab/src/app/tabs/tabs-routing.module.ts
+++ co-fit-rehab/src/app/tabs/tabs-routing.module.ts
@@ -12,8 +12,8 @@ const routes: Routes = [
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'latihan',
+        loadChildren: () => import('../latihan/latihan.module').then(m => m.LatihanPageModule)
       },
       {
         path: 'tab3',
@@ -21,14 +21,14 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/latihan',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/latihan',
     pathMatch: 'full'
   }
 ];
