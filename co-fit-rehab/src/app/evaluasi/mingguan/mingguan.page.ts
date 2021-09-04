9e83c0ce66be8ee80e6321988e4b557447a15284
nofaulia <nofaulia@gmail.com>
2021-09-04 08:00:56 +0700

    add weekly evaluation chart

--- co-fit-rehab/src/app/evaluasi/mingguan/mingguan.page.ts
+++ co-fit-rehab/src/app/evaluasi/mingguan/mingguan.page.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Inject, NgZone } from '@angular/core';
+import { Component, OnInit, Inject, NgZone, ViewChild, ElementRef } from '@angular/core';
 
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder  } from "@angular/forms";
 import { WeeklyEvaluationService } from './../../services/weekly-evaluation.service';
 import { UserdataService } from './../../services/userdata.service';
 
+import { Chart, registerables  } from "chart.js";
 
 @Component({
   selector: 'app-mingguan',
@@ -13,22 +14,143 @@ import { UserdataService } from './../../services/userdata.service';
 })
 
 export class MingguanPage implements OnInit {
+  @ViewChild("rhrCanvas") rhrCanvas: ElementRef;
+  @ViewChild("bfiCanvas") bfiCanvas: ElementRef;
+  @ViewChild("stsCanvas") stsCanvas: ElementRef;
+
   Evaluation: any = [];
+
+  private rhrChart: Chart;
+  private bfiChart: Chart;
+  private stsChart: Chart;
 
   constructor(
     private evalService: WeeklyEvaluationService,
     private userdataService: UserdataService,
     private router: Router
-  ) { }
+  ) { Chart.register(...registerables); }
 
   ngOnInit() {
   }
 
   ionViewDidEnter() {
+    var labels = [];
+    var datasetRHR = [];
+    var datasetBFI = [];
+    var datasetSTS = [];
+
     this.userdataService.getPatientId().then((patientId) => {
       this.evalService.getEvaluations(patientId).subscribe((response) => {
         this.Evaluation = response;
         console.log('**response ' + this.Evaluation);
+
+        response['data'].forEach((item) => {
+          labels.push(item['tanggal'].split(" ")[0]);
+          datasetRHR.push(item['rhr']);
+          datasetBFI.push(item['bfi']);
+          datasetSTS.push(item['sts30detik']);
+        });
+
+        // create line chart
+        this.rhrChart = new Chart(this.rhrCanvas.nativeElement, {
+      type: "line",
+      data: {
+        labels: labels,
+        datasets: [
+        {
+          label: "RHR", // BFI
+          fill: false,
+          // lineTension: 0.1,
+          backgroundColor: "rgba(225,0,0,0.4)",
+          borderColor: "red", // The main line color
+          borderCapStyle: 'square',
+          borderDash: [], // try [5, 15] for instance
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: "black",
+          pointBackgroundColor: "white",
+          pointBorderWidth: 1,
+          pointHoverRadius: 8,
+          pointHoverBackgroundColor: "yellow",
+          pointHoverBorderColor: "brown",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 5,
+          // notice the gap in the data and the spanGaps: true
+          data: datasetRHR,
+          spanGaps: true,
+        }
+        ]
+      }
+    });
+
+        // create line chart
+        this.bfiChart = new Chart(this.bfiCanvas.nativeElement, {
+      type: "line",
+      data: {
+        labels: labels,
+        datasets: [
+        {
+          label: "BFI", // BFI
+          fill: false,
+          // lineTension: 0.1,
+          backgroundColor: "rgba(225,0,0,0.4)",
+          borderColor: "red", // The main line color
+          borderCapStyle: 'square',
+          borderDash: [], // try [5, 15] for instance
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: "black",
+          pointBackgroundColor: "white",
+          pointBorderWidth: 1,
+          pointHoverRadius: 8,
+          pointHoverBackgroundColor: "yellow",
+          pointHoverBorderColor: "brown",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 5,
+          // notice the gap in the data and the spanGaps: true
+          data: datasetBFI,
+          spanGaps: true,
+        }
+        ]
+      }
+    });
+
+        // create line chart
+        this.stsChart = new Chart(this.stsCanvas.nativeElement, {
+      type: "line",
+      data: {
+        labels: labels,
+        datasets: [
+        {
+          label: "STS 30 Detik", // STS
+          fill: false,
+          // lineTension: 0.1,
+          backgroundColor: "rgba(225,0,0,0.4)",
+          borderColor: "red", // The main line color
+          borderCapStyle: 'square',
+          borderDash: [], // try [5, 15] for instance
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: "black",
+          pointBackgroundColor: "white",
+          pointBorderWidth: 1,
+          pointHoverRadius: 8,
+          pointHoverBackgroundColor: "yellow",
+          pointHoverBorderColor: "brown",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 5,
+          // notice the gap in the data and the spanGaps: true
+          data: datasetSTS,
+          spanGaps: true,
+        }
+        ]
+      }
+    });
+
+
       })  
     });
   }
